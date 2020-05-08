d7fa17e0f33e737a1955a03697ee76a80248c56a
Zsolt <zsolt@u-tx.com>
2020-05-08 12:30:24 +0300

    first commit, example image is shown as static

---
+++ screensaver.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2013 Tristan Fischer (sphere@dersphere.de)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import xbmcaddon
+import xbmcgui
+import xbmc
+
+addon = xbmcaddon.Addon()
+addon_name = addon.getAddonInfo('name')
+addon_path = addon.getAddonInfo('path')
+
+
+#CONTROL_MOVING_BACKGROUND = 1
+#CONTROL_ANIMATED_RAINBOW = 2
+
+
+class Screensaver(xbmcgui.WindowXMLDialog):
+
+    class ExitMonitor(xbmc.Monitor):
+
+        def __init__(self, exit_callback):
+            self.exit_callback = exit_callback
+
+        def onScreensaverDeactivated(self):
+            self.exit_callback()
+
+    def onInit(self):
+        self.exit_monitor = self.ExitMonitor(self.exit)
+        self.handle_settings()
+
+    def handle_settings(self):
+        self.interval = addon.getSetting('refresh_interval')
+
+    def exit(self):
+        self.abort_requested = True
+        self.exit_monitor = None
+        self.log('exit')
+        self.close()
+        
+    #def mainLoop(self):
+        #do some shit
+        
+        
+
+    def log(self, msg):
+        xbmc.log(u'Grafana Screensaver: %s' % msg)
+
+
+if __name__ == '__main__':
+    screensaver = Screensaver(
+        'script-Grafana-main.xml',
+        addon_path,
+        'default',
+    )
+    screensaver.doModal()
+    del screensaver
+    sys.modules.clear()
