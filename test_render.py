feb94376dae099994005918651ff78e37f78a9cd
Zsolt <zsolt@u-tx.com>
2021-06-03 16:17:33 +0300

    test with rendertron , much better performance then grafana renderer..

--- test_render.py
+++ test_render.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
-import requests
+import urllib
 import re
 from pprint import pprint
+import sys
 
 URL = 'http://nuc:3000/render/d/q4jAnx6Zk/zeenet?from=now-3h&to=now&width=1920&height=1080'
-
+URL_RENDERTRON='http://192.168.1.11:3001/screenshot/http://nuc:3000/d/VLVPGnRMz/zeenet2?orgId=1&refresh=5s'
+TMP_IMG="/tmp/grafana.png"
 
 def getLatestRendering():
     page = requests.get(URL)
     content = page.content
-    
+
     with open("/tmp/grafana.png", 'wb') as fd:
         fd.write(content)
         fd.close()
-        
+
+
+def getLatestRendering2(url, tempPicture):
+    try:
+
+        image_on_web = urllib.urlopen(url)
+        if image_on_web.headers.maintype == 'image':
+            buf = image_on_web.read()
+            downloaded_image = file(tempPicture, "wb")
+            downloaded_image.write(buf)
+            downloaded_image.close()
+            image_on_web.close()
+        else:
+            return False
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except:
+        return False
+    return True
 
 def mainLoop():
     while True:
         print("Getting rendering")
-        getLatestRendering()
-        
-        
+        getLatestRendering2(URL_RENDERTRON, TMP_IMG)
+
 mainLoop()
